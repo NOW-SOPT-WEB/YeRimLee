f48ac3551af032a9e784f32e88ae6160bdca7daf
이예림 <yhli0320@naver.com>
2024-04-14 02:04:00 +0900

    test: 미완성 코드 저장

---
+++ week2/연습장.js
@@ -0,0 +1,16 @@
+//li 태그 만들어서 시작화면에 '전체' 추가
+//const all = document.createElement("h2");
+//all.textContent = "전체";
+
+//const itemList = document.querySelector(".item_list");
+
+//itemList.appendChild(all);
+
+//div 태그 만들어서 시작화면에 상품 나열
+//const all_list = document.createElement("div");
+
+//const imgBox = document.querySelector(".item_box");
+
+//imgBox.append("닥터자르트", 24000);
+//html꺼 태그 삭제함
+//연결 끊음
