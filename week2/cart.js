9caa248c3ac5755c2ab4bdf76bce976ca47d2805
이예림 <yhli0320@naver.com>
2024-04-14 02:40:03 +0900

    feat: 클릭시 장바구니 이동

--- week2/cart.js
+++ week2/cart.js
@@ -1,3 +1,12 @@
+//item 클릭시 장바구니에 담기 alert가 뜨고 yes 누르면 장바구니로 이동됨
+const item = document.querySelector(".section");
+const itemClickHandler = () => {
+  alert("장바구니 담기");
+  const cartPlus = "cart.html";
+  location.href = cartPlus;
+};
+item.addEventListener("click", itemClickHandler);
+
 //li 태그 만들어서 시작화면에 '전체' 추가
 //const all = document.createElement("h2");
 //all.textContent = "전체";
