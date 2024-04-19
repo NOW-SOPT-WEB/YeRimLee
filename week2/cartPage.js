e6a0542b4348c6929ea2f73de5f148ecbd9c38ba
이예림 <yhli0320@naver.com>
2024-04-19 20:37:13 +0900

    feat: 장바구니 모달 기초

--- week2/cartPage.js
+++ week2/cartPage.js
@@ -45,12 +45,28 @@ deleteBtn.forEach((item) => {
   item.addEventListener("click", handleDeleteBtn);
 });
 
+//여기서부터 모달!
+
 function openModal() {
   const modal = document.querySelector(".purchase_modal");
-
   modal.showModal();
+  //모달에 장바구니 리스트 뿌려주기
+  const showCart = document.querySelector(".modal_product_list_wrapper");
+
+  let showCartList = cartList.map((item) => {
+    return `
+       <img id=${item.id} class="modal_img" src="${item.Image}" alt="${item.name}">
+       <h3>${item.name}</h3>
+       <p>${item.price}원</p>
+      `;
+  });
+  showCart.innerHTML += showCartList.join("");
 }
-
 const purchaseBtn = document.querySelector(".purchase_btn");
-
 purchaseBtn.addEventListener("click", openModal);
+
+//모달 내 구매하기 버튼 추가 -> 클릭시 완료 알림창과 함께 모달 닫기
+//모달 내 총 금액 표시
+// const modal_purchaseBtn = document.createElement("button");
+// modal_purchaseBtn.textContent = "구매하기";
+// modal_purchaseBtn.className = "purchase_item";
