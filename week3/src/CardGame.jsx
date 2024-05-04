2d9bded1889366a0339c8693297920aeca3dba65
yerim lee <yhli0320@naver.com>
2024-05-04 18:44:28 +0900

    feat: 카드 뒤집기

--- week3/src/CardGame.jsx
+++ week3/src/CardGame.jsx
@@ -1,65 +1,84 @@
-import img6 from "../public/img/6.png"
+import img6 from "../public/img/6.png";
 import Header from "./Header";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import CARD_LIST from "./assets/card";
-import { useState } from 'react'
-
+import { useState } from "react";
 
 const CardWrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    padding: 5rem;
-    width: 60rem;
-
+  display: flex;
+  flex-wrap: wrap;
+  padding: 5rem;
+  width: 60rem;
 `;
 const PairCard = styled.div`
-    position: relative;
-    width: 10rem;
-    height: 16rem;
-    padding: 0.5rem;
-`
+  position: relative;
+  width: 10rem;
+  height: 16rem;
+  padding: 0.5rem;
+`;
 const CardImg = styled.img`
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
 
-    border-radius: 8%;
+  border-radius: 8%;
 `;
 
-const  CoverImg = styled(CardImg)`
-    position: absolute;
-    left: 0;
-`
-const MixedCardList = CARD_LIST.sort(() => 0.5 - Math.random());
-console.log(MixedCardList);
+const CoverImg = styled(CardImg)`
+  position: absolute;
+  left: 0;
+`;
+//카드 랜덤으로 섞기
+CARD_LIST.sort(() => 0.5 - Math.random());
 
+const CardGame = () => {
+  //카드상태
+  const [isOpen, setOpen] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+  const OpenCard = (index) => {
+    //isOpen[index]를 활용하자..!
+    //console.log(index);
+    //console.log("온클릭 함수 작동");
+    const arr = [...isOpen];
+    //console.log(arr);
+    arr[index] = true;
+    setOpen(arr);
 
+    //console.log(arr);
+  };
 
-
-const CardGame = () => { 
-    const [isOpen, setOpen] = useState(false); 
-
-    const OpenCard = () => {
-        setOpen(true);
-    }
-
-    return (
-        <div>
-            <Header  />
-            <CardWrapper>
-                {
-                    CARD_LIST.map((item, index) => {
-                        return (
-                            <PairCard key={index}>
-                                <CardImg className="showCard_img" src={item.imgSrc} />
-                                {isOpen ? <></> : <CoverImg className="closed_card" onClick={OpenCard} src={img6} />}
-                            </PairCard>
-
-                        )
-                    })
-                }
-            </CardWrapper>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <CardWrapper>
+        {CARD_LIST.map((item, index) => {
+          return (
+            <PairCard key={index}>
+              <CardImg className="showCard_img" src={item.imgSrc} />
+              {isOpen[index] ? (
+                <></>
+              ) : (
+                <CoverImg
+                  className="closed_card"
+                  onClick={() => OpenCard(index)}
+                  src={img6}
+                />
+              )}
+            </PairCard>
+          );
+        })}
+      </CardWrapper>
+    </div>
+  );
 };
 export default CardGame;
--- week3/src/assets/card.js
+++ week3/src/assets/card.js
@@ -1,27 +1,54 @@
-export const CARD_LIST = [
+const CARD_LIST = [
     {
         id: 1,
         imgAlt: "쿠루미",
-        imgSrc: "src/assets/images/1.png",
+        imgSrc: "../public/img/1.png",
     },
     {
         id: 2,
         imgAlt: "마이멜로디",
-        imgSrc: "src/assets/images/1.png",
+        imgSrc: "../public/img/2.png",
     },
     {
         id: 3,
         imgAlt: "포챠코",
-        imgSrc: "src/assets/images/1.png",
+        imgSrc: "../public/img/3.png",
     },
     {
         id: 4,
         imgAlt: "케로케로케로피",
-        imgSrc: "src/assets/images/1.png",
+        imgSrc: "../public/img/4.png",
     },
     {
         id: 5,
         imgAlt: "배드바츠마루",
-        imgSrc: "src/assets/images/1.png",
+        imgSrc: "../public/img/5.png",
+    },
+    {
+        id: 1,
+        imgAlt: "쿠루미",
+        imgSrc: "../public/img/1.png",
+    },
+    {
+        id: 2,
+        imgAlt: "마이멜로디",
+        imgSrc: "../public/img/2.png",
+    },
+    {
+        id: 3,
+        imgAlt: "포챠코",
+        imgSrc: "../public/img/3.png",
+    },
+    {
+        id: 4,
+        imgAlt: "케로케로케로피",
+        imgSrc: "../public/img/4.png",
+    },
+    {
+        id: 5,
+        imgAlt: "배드바츠마루",
+        imgSrc: "../public/img/5.png",
     },
 ];
+
+export default CARD_LIST;
